393695629a8e99b28da980a9e9ce84d7e5a417ff
Kyle Slattery <kyle@kyleslattery.com>
2010-01-18 21:48:18 -0500

    use technoweenie/ntest for tests and write the first one

--- test/xml2object_test.js
+++ test/xml2object_test.js
@@ -1,26 +1,39 @@
 var sys = require('sys'),
 	assert = require('assert');
 	xml2object = require('../lib/xml2object');
+	
+process.mixin(GLOBAL, require('../vendor/ntest/lib/index'));
 
-var tests = [];
+describe("Basic XML String parse with parseString")
+	before(function() {
+		this.xml = "<root><item name=\"value\">text</item></root>";
+		this.response = xml2object.parseString(this.xml);
+	})
 
-// Basic XML
-tests.push(function() {
-	var xml = "<root><item name=\"value\">text</item></root>";
-	var expected = {
-		root: {
-			item: {
-				attrs: {
-					name: "value"
-				},
-				content: "text"
-			}
-		}
-	};
-
-	assert.equal(xml2object.parseString(xml), expected, "Should parse basic XML string");
-});
-
-for(var i=0; i<tests.length; i++) {
-	tests[i]();
-}
+	it("should return EventEmitter", function() {
+		sys.puts(this.response.constructor.toString().match(/eventemitter/i));
+		assert.notEqual(this.response.constructor.toString().match(/eventemitter/i), null);
+	})
+	
+// 
+// var tests = [];
+// 
+// // Basic XML
+// tests.push(function() {
+// 	var expected = {
+// 		root: {
+// 			item: {
+// 				attrs: {
+// 					name: "value"
+// 				},
+// 				content: "text"
+// 			}
+// 		}
+// 	};
+// 
+// 	assert.equal(xml2object.parseString(xml), expected, "Should parse basic XML string");
+// });
+// 
+// for(var i=0; i<tests.length; i++) {
+// 	tests[i]();
+// }
